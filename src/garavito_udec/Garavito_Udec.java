613ea470fc9033cd0c8d82503241342ecd4f4ddc
Garavito_Udec <cmateoramirez@ucundinamarca.edu.co>
2022-10-07 21:24:59 -0500

    Rama principal

---
+++ src/garavito_udec/Garavito_Udec.java
@@ -0,0 +1,12 @@
+package garavito_udec;
+/* @author Garavito_Udec */
+public class Garavito_Udec {
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Garavito_UdecF().setVisible(true);
+            }
+        });
+    }
+    
+}
